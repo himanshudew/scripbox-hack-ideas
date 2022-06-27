a61feeabd73c627f409ad05b81c22c8c2b9fa2d6
Himanshu Dewangan <himanshu.dewangan@yahoo.com>
2022-06-27 16:00:51 +0530

    added unti test case for login and app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs/internal/observable/of';
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let authService: AuthService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -14,22 +20,37 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.get(AuthService);
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'scripbox-hack-ideas'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('scripbox-hack-ideas');
+    expect(component.title).toEqual('scripbox-hack-ideas');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('scripbox-hack-ideas app is running!');
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('scripbox-hack-ideas');
+  });
+
+  it('#ngOnint() user not logged in', () => {
+    component.ngOnInit();
+    expect(component.isUserLoggedIn).toBeFalse();
+  });
+
+  it('#ngOnint() user logged in', () => {
+    component.ngOnInit();
+    component.isUserLoggedIn = true;
+    fixture.detectChanges();
+    expect(component.isUserLoggedIn).toBeTrue();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.btn').textContent).toContain('LogOut');
   });
 });
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,25 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../services/auth.service';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [LoginComponent],
+      providers: [FormBuilder]
+    }).compileComponents();
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.get(AuthService);
     fixture.detectChanges();
+    spyOn(authService,'login').and.callThrough();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update userid from form changes and loginForm valid true', fakeAsync(() => {
+    component.loginForm.controls['userid'].setValue('admin');
+    expect(component.form.userid.value).toEqual('admin');
+    expect(component.loginForm.invalid).toBeFalse();
+  }));
+
+  it('if userid is empty, loginForm valid to be false', fakeAsync(() => {
+    component.loginForm.controls['userid'].setValue('');
+    expect(component.loginForm.invalid).toBeTrue();
+  }));
+
+  it('#login when userid is empty', () => {
+    component.loginForm.controls['userid'].setValue('');
+    component.login();
+    expect(component.message).toBe('Please input your userid.');
+  });
+
+  it('#login when userid is notadmin', () => {
+    component.loginForm.controls['userid'].setValue('notadmin');
+    component.login();
+    expect(component.message).toBe('Please check your userid.');
+  });
+
+  it('#login when userid is admin', () => {
+    component.loginForm.controls['userid'].setValue('admin');
+    component.login();
+    expect(authService.login).toHaveBeenCalledWith('admin');
   });
 });
